582390e847ba7fc4442b2e9272dc97d44bde27c6
alexciotor <ciotor.alex@yahoo.com>
2020-11-19 10:37:35 +0200

    First Take of John's Method

---
+++ src/presentDrinks.js
@@ -0,0 +1,17 @@
+import fetchDrinks from './fetchDrinks.js'
+import displayDrinks from './displayDrinks.js'
+import setDrink from './setDrink.js'
+const showDrinks = async(url)=>{
+  const data = await fetchDrinks(url)
+//  fetch drinks
+ 
+// display drinks
+    const section = displayDrinks(data)
+    if(section){
+        setDrink(section)
+    }
+   
+
+}
+
+export default showDrinks
